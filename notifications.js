9311701c8ea00fe55cf5b2ee8452fc2adabbd5e7
linafillippova <158497409+linafillippova@users.noreply.github.com>
2024-12-06 22:43:25 +0300

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -63,41 +63,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const notification = document.createElement("div");
     notification.className = "notification";
-    notification.style.position = "fixed";
-    notification.style.top = "50%";
-    notification.style.left = "50%";
-    notification.style.transform = "translate(-50%, -50%)";
-    notification.style.padding = "20px";
-    notification.style.backgroundColor = "#f8d7da";
-    notification.style.color = "#721c24";
-    notification.style.border = "1px solid #f5c6cb";
-    notification.style.borderRadius = "10px";
-    notification.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
-    notification.style.zIndex = "1000";
-    notification.style.textAlign = "center";
-
+    
     const messageElement = document.createElement("p");
     messageElement.textContent = message;
     notification.appendChild(messageElement);
 
     const button = document.createElement("button");
     button.textContent = "Окей";
-    button.style.marginTop = "10px";
-    button.style.padding = "10px 20px";
-    button.style.backgroundColor = "#f5c6cb";
-    button.style.color = "#721c24";
-    button.style.border = "none";
-    button.style.borderRadius = "5px";
-    button.style.cursor = "pointer";
-    button.style.transition = "background-color 0.3s, color 0.3s";
-
+    
     button.addEventListener("mouseover", () => {
-      button.style.backgroundColor = "#721c24";
-      button.style.color = "#fff";
+      
     });
     button.addEventListener("mouseout", () => {
-      button.style.backgroundColor = "#f5c6cb";
-      button.style.color = "#721c24";
+      
     });
 
     button.addEventListener("click", () => {
